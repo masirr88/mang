98f44d6835cc6afec661d7791f02a3b84d2e32f5
masirr88 <43272436+masirr88@users.noreply.github.com>
2026-05-01 13:44:25 +0800

    Add files via upload

---
+++ 溪湖忠覺馬氏家族/js/app.js
@@ -0,0 +1,42 @@
+    let nextBtn = document.querySelector('.next') 
+    let prevBtn = document.querySelector('.prev')
+    
+    let slider = document.querySelector('.slider')
+    let sliderList = slider.querySelector('.slider .list')
+    
+    let thumbnail = document.querySelector('.thumbnail')
+    let thumbnailItems = thumbnail.querySelectorAll('.item')
+
+    thumbnail.appendChild(thumbnailItems[0])
+  
+    nextBtn.onclick = function() {
+        moveSlider('next')
+    }
+
+
+    prevBtn.onclick = function() {
+        moveSlider('prev')
+    }
+
+    function moveSlider(direction) {
+        let sliderItems = slider.querySelectorAll('.item') 
+        let thumbnailItems = document.querySelectorAll('.thumbnail .item')
+
+        if(direction === 'prev'){
+            sliderList.appendChild(sliderItems[1])
+            thumbnail.appendChild(thumbnailItems[1])
+            slider.classList.add('prev')        
+        } else {
+            sliderList.appendChild(sliderItems[-0])
+            thumbnail.appendChild(thumbnailItems[-0])
+            slider.classList.add('next')        
+        }
+    }
+
+    slider.addEventListener('animationend', function() {
+        if(direction === 'next'){
+            slider.classList.remove('prev')
+        } else {
+            slider.classList.remove('next')
+        }
+    }, {once: true})
